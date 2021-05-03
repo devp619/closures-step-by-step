3e04d79c354f054bb8dbbd2dac1eb6ed53ba7d88
Debasish <dpanda04@in.ibm.com>
2021-05-03 22:52:59 +0530

    Added uses of closure

--- Closures.js
+++ Closures.js
@@ -8,7 +8,7 @@ function superOuterFunction() {
       console.log(superOuterText + outerText + innerText + argumentText);
     }
     // As long as outerText sits in the same lexical environment, its available to innerFunction
-    //     let outerText = "I am declared in outer function, ";
+    let outerText = "I am declared in outer function, ";
     return innerFunction;
   }
   return outerFunction;
@@ -22,6 +22,18 @@ function superOuterFunction() {
 
 // OR
 
-let outerText = "I am declared in global scope, ";
-
 superOuterFunction()("even after the outer function has finished executing")();
+
+
+// let outerText = "I am declared in global scope, ";
+
+/*
+Uses of closure:
+- Module pattern
+- Function currying
+- Data hiding/encapsulation
+- Memoization
+- State management in async operations
+- setTimeOut
+- Iterators
+*/
