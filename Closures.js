86c7c131f47b3be2adbf59009e08747ffe03c476
Debasish <dpanda04@in.ibm.com>
2021-05-03 22:38:34 +0530

    Added an additional outer function to show scope chain

--- Closures.js
+++ Closures.js
@@ -1,18 +1,25 @@
-function outerFunction(argumentText) {
-  //   Lexical environment surrounding 'innerFunction'
-  function innerFunction() {
-    var innerText = "yet accessible from inner function, ";
-    console.log(outerText + innerText + argumentText);
+function superOuterFunction() {
+  //   Lexical environment surrounding 'outerFunction'
+  var superOuterText = "I am declared in the outer most function\n";
+  function outerFunction(argumentText) {
+    //   Lexical environment surrounding 'innerFunction'
+    function innerFunction() {
+      var innerText = "yet accessible from inner function, ";
+      console.log(superOuterText + outerText + innerText + argumentText);
+    }
+    // As long as outerText sits in the same lexical environment, its available to innerFunction
+    let outerText = "I am declared in outer function, ";
+    return innerFunction;
   }
-  // As long as outerText sits in the same lexical environment, its available to innerFunction
-  let outerText = "I am declared in outer function, ";
-  return innerFunction;
+  return outerFunction;
 }
 
-var innerFunctionGlobal = outerFunction("even after the outer function has finished executing"); 
+// var outerFunctionGlobal = superOuterFunction();
 
-innerFunctionGlobal();
+// var innerFunctionGlobal = outerFunctionGlobal("even after the outer function has finished executing");
+
+// innerFunctionGlobal();
 
 // OR
 
-// outerFunction("even after the outer function has finished executing")(); 
+superOuterFunction()("even after the outer function has finished executing")(); 
